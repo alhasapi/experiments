7ed6255938c75b6755e285fb3aa30f9f631842c5
MOKTAR ALHASAPI <moktaralhasapi@gmail.com>
2021-01-27 20:22:15 +0100

    Update grab_tweets.py

--- grab_tweets.py
+++ grab_tweets.py
@@ -1,10 +1,7 @@
 from twitter import *
 from pprint import pprint as p
 
-t = Twitter(auth=OAuth("833474190254698496-0yy50CdwitB8vWPe8G7BTU8t8jdie0W", 
-                       "tEV13P7yyslqOx4XOEyfWdi70gnsGo3MgXItyeID5fPva",                 
-                       "kaPKNaydiko9PzKMxqBd0j8nU", 
-                       "DT4QK2T3abnDnC2lLholx5Dq2yTin36TdV9fNiInl8n2yakCfb"))
+t = Twitter(auth=OAuth())
                        
 result = t.geo.search(query="Cameroon", granularity="country")
 for i in range(len(result['result']['places'])):
